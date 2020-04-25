0bb22764c5a33b3ec24feb3fa9f4ff848db17352
Paweł Pustelnik <pustypawel@gmail.com>
2020-04-25 16:23:07 +0200

    server port property

--- src/main/java/pl/edu/wszib/labordersspring/LabOrdersSpringApplication.java
+++ src/main/java/pl/edu/wszib/labordersspring/LabOrdersSpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LabOrdersSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LabOrdersSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LabOrdersSpringApplication.class, args);
+    }
 
 }

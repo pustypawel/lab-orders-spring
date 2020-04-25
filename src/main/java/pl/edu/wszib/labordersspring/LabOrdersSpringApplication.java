402f1f4bcac3934b2bd18858a842b5a30685f8d8
Paweł Pustelnik <pustypawel@gmail.com>
2020-04-25 16:20:15 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/labordersspring/LabOrdersSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.labordersspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabOrdersSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabOrdersSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/wszib/labordersspring/LabOrdersSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.labordersspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabOrdersSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

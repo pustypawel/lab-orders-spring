c4593b384b6f97562c4b09037ba1ecbd2780bb71
Paweł Pustelnik <pustypawel@gmail.com>
2020-04-25 17:29:36 +0200

    partionally working create orders

--- src/main/java/pl/edu/wszib/labordersspring/rest/OrderController.java
+++ src/main/java/pl/edu/wszib/labordersspring/rest/OrderController.java
@@ -1,15 +1,23 @@
 package pl.edu.wszib.labordersspring.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.wszib.labordersspring.order.Order;
+import pl.edu.wszib.labordersspring.order.OrderService;
 
 @RequestMapping("/api/orders")
 @RestController
 public class OrderController {
+    private final OrderService orderService;
 
-    @GetMapping
-    public String helloOrders() {
-        return "Hello orders";
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public String create(@RequestBody Order order) {
+        return orderService.create(order);
     }
 }

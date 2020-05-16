30793071951d170bb43e5f2c026ebc097aa6ef31
Paweł Pustelnik <pustypawel@gmail.com>
2020-05-16 16:34:58 +0200

    dto jako immutable object

--- src/main/java/pl/edu/wszib/labordersspring/rest/api/OrderCreateDto.java
+++ src/main/java/pl/edu/wszib/labordersspring/rest/api/OrderCreateDto.java
@@ -1,45 +1,39 @@
 package pl.edu.wszib.labordersspring.rest.api;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 public class OrderCreateDto {
-    private List<Position> positions;
+    private final List<Position> positions;
 
-    public List<Position> getPositions() {
-        return positions;
-    }
-
-    public void setPositions(List<Position> positions) {
+    @JsonCreator
+    public OrderCreateDto(@JsonProperty("positions") List<Position> positions) {
         this.positions = positions;
     }
 
     public static class Position {
-        private Integer quantity;
-        private Item item;
+        private final Integer quantity;
+        private final Item item;
 
-        public Integer getQuantity() {
-            return quantity;
-        }
-
-        public void setQuantity(Integer quantity) {
+        @JsonCreator
+        public Position(@JsonProperty("quantity") Integer quantity,
+                        @JsonProperty("item") Item item) {
             this.quantity = quantity;
-        }
-
-        public Item getItem() {
-            return item;
-        }
-
-        public void setItem(Item item) {
             this.item = item;
         }
 
         public static class Item {
-            private String name;
-            private BigDecimal price;
-            private Integer stock;
+            private final String name;
+            private final BigDecimal price;
+            private final Integer stock;
 
-            public Item(String name, BigDecimal price, Integer stock) {
+            @JsonCreator
+            public Item(@JsonProperty("name") String name,
+                        @JsonProperty("price") BigDecimal price,
+                        @JsonProperty("stock") Integer stock) {
                 this.name = name;
                 this.price = price;
                 this.stock = stock;

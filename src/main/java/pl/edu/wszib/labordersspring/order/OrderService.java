23e4250ae085c9bf7e13e8e61f4faaf67dd9c7bc
Paweł Pustelnik <pustypawel@gmail.com>
2020-06-20 17:38:49 +0200

    transaction management

--- src/main/java/pl/edu/wszib/labordersspring/order/OrderService.java
+++ src/main/java/pl/edu/wszib/labordersspring/order/OrderService.java
@@ -1,11 +1,18 @@
 package pl.edu.wszib.labordersspring.order;
 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import pl.edu.wszib.labordersspring.rest.api.OrderDto;
 
 import java.util.Collection;
 
+@Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
+
+    // manual transaction management
+//    @Autowired
+//    private PlatformTransactionManager transactionManager;
 
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
@@ -15,24 +22,35 @@ public class OrderService {
         return orderRepository.save(order);
     }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public boolean exists(String orderId) {
         return orderRepository.exists(orderId);
     }
 
     public void addPosition(String orderId, Position position) {
+        // manual transaction management
+//        TransactionStatus transaction = transactionManager.getTransaction(new DefaultTransactionDefinition());
+//        try {
+//            transactionManager.commit(transaction);
+//        } catch (Exception e) {
+//            transactionManager.rollback(transaction);
+//        }
         Order order = orderRepository.load(orderId);
         order.addPosition(position);
         orderRepository.save(order);
     }
 
+    @Transactional(readOnly = true)
     public Order find(String orderId) {
         return orderRepository.load(orderId);
     }
 
+    @Transactional(readOnly = true)
     public Collection<Order> getAll() {
         return orderRepository.getAll();
     }
 
+    @Transactional(readOnly = true)
     public OrderDto getOne(String orderId) {
         // Przykład jak wywołać OrderErrorHandler.handleUnexpectedException
 //        Order order = null;
